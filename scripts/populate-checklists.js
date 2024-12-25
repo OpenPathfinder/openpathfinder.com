b119f716274e04e00dd7930adf671f1a3aeb83de
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2024-12-25 18:39:55 +0100

    Add a checklists index (#16)

--- scripts/populate-checklists.js
+++ scripts/populate-checklists.js
@@ -1,1 +1,57 @@
-console.log('Not yet implemented. Skipping...');
+const { writeFileSync, existsSync, readFileSync } = require('fs')
+const { updateOrCreateSegment } = require('@ulisesgascon/text-tags-manager')
+const path = require('path')
+
+const checklists = require('../data/checklists.json')
+
+const listStartTag = '<!-- LIST:START -->'
+const listEndTag = '<!-- LIST:END -->'
+
+const sortByPriority = (a, b) => {
+    const [aLetter, aNumber] = a.priority_group.match(/[A-Z]+|\d+/g);
+    const [bLetter, bNumber] = b.priority_group.match(/[A-Z]+|\d+/g);
+    return aLetter.localeCompare(bLetter) || aNumber - bNumber;
+}
+
+const slugify = (text) => {
+    //remove dots and convert to lowercase
+    return text.replace(/\./g, '').toLowerCase()
+}
+
+const intro = checklists.map(checklist => {
+    return `- [${checklist.author}: ${checklist.title}](/docs/visionBoard/checklists#${slugify(checklist.code_name)})`
+}).join('\n')
+
+checklistsListContent = `## Official Checklists
+
+Here you can find the official checklists for the Vision Board project:
+${intro}
+
+${checklists.map(checklist => {
+return `### ${checklist.code_name}
+- **Author**: ${checklist.author}
+- **Title**: ${checklist.title}
+- **Description**: ${checklist.description}
+
+| Priority | Check | details | Info |
+| --- | --- | --- | --- |
+${checklist.checks
+    .sort(sortByPriority)
+    .map(check => {
+        return `| ${check.priority_group} | ${check.code_name} | ${check.title} | [Doc](/docs/checks/${check.code_name}) |`.trim()
+    }).join('\n')}
+    `
+}).join('\n')}
+`
+
+const checklistsListsDestination = path.join(process.cwd(), 'docs/projects/visionBoard/checklists.md')
+checklistsListFileContent = readFileSync(checklistsListsDestination, 'utf8')
+
+checklistsListFileContent = updateOrCreateSegment({
+    original: checklistsListFileContent,
+    replacementSegment: checklistsListContent,
+    startTag: listStartTag,
+    endTag: listEndTag
+})
+
+writeFileSync(checklistsListsDestination, checklistsListFileContent)
